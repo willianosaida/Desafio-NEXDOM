674e521247caaa7cce8cce53bd22cc205452a253
Willian Alberto Osaida <64991059+willianosaida@users.noreply.github.com>
2026-08-30 00:06:15 -0300

    Removing obsolete folders

--- cypress/pages/solucoes.page.js
+++
@@ -1,8 +0,0 @@
-class SolucoesPage {
-  validarPaginaDeSolucao() {
-    cy.url().should('include', '/gestao-de-planos-de-saude/');
-    cy.title().should('contain', 'Gestão de planos de saúde');
-  }
-}
-
-module.exports = new SolucoesPage();
